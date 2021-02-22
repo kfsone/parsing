32fd323b067e5afadf4b3b2268ad8b31a3dc06f3
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-02-22 15:58:35 -0800

    wrap findfiles functions in timing

--- parsefiles.go
+++ parsefiles.go
@@ -21,25 +21,27 @@ func ParseFiles(extension string, parseFn func(string), pathlist []string) {
 	workQueue := make(chan string, numWorkers+1)
 
 	// Start finding files in the background.
-	go FindFiles(pathlist, extension, workQueue)
+	go stats.Time("parsing.findfiles", true, func () { FindFiles(pathlist, extension, workQueue) })
 
 	// Create a set of workers to consume filenames and parse them.
-	workers.Add(numWorkers)
-	for i := 0; i < numWorkers; i++ {
-		// Worker implementation
-		go func() {
-			// reduce worker count by one when this function exits.
-			defer workers.Done()
+	stats.Time("parsing.startworkers", true, func () {
+		workers.Add(numWorkers)
+		for i := 0; i < numWorkers; i++ {
+			// Worker implementation
+			go func() {
+				// reduce worker count by one when this function exits.
+				defer workers.Done()
 
-			for filepath := range workQueue {
-				if *stats.Verbose > 1 {
-					stats.Time(filepath, false, func() { parseFn(filepath) })
-				} else {
-					parseFn(filepath)
+				for filepath := range workQueue {
+					if *stats.Verbose > 1 {
+						stats.Time(filepath, false, func() { parseFn(filepath) })
+					} else {
+						parseFn(filepath)
+					}
 				}
-			}
-		}()
-	}
+			}()
+		}
+	})
 
 	// Wait for the work to complete.
 	workers.Wait()
